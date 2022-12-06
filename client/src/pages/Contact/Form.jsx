0204565cadd839d39ac24ee26bd09cd469c37970
cspencer <cspencernd@gmail.com>
2022-12-05 23:22:49 -0500

    add some validation and a bunch of styling

--- client/src/pages/Contact/Form.jsx
+++ client/src/pages/Contact/Form.jsx
@@ -1,6 +1,22 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 import axios from "./axios"
+import "./Form.css"
+
+const schema = yup.object().shape({
+    name: yup
+        .string()
+        .matches("^([^0-9]*)$", "Name should not contain numbers")
+        .required("Name is a required field"),
+    email: yup
+        .string()
+        .email("Not a valid email")
+        .required("Email is a required field"),
+    phone: yup.string().required("Phone is a required field"),
+    description: yup.string().required("Description is a required field"),
+})
 
 const Form = () => {
     const REGISTER_URL = "/send"
@@ -12,66 +28,51 @@ const Form = () => {
         handleSubmit,
         reset,
         formState: { errors },
-    } = useForm()
+    } = useForm({ mode: "onBlur", resolver: yupResolver(schema) })
 
     const onSubmit = (data) => {
         console.log(data)
     }
 
-    const Error = ({ err }) => {
-        return (
-            <div className="alert alert-error shadow-box p-2">
-                <div>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="stroke-current flex-shrink-0 h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
-                    <span>{`${err} is a required field.`}</span>
-                </div>
-            </div>
-        )
-    }
-
     return (
         <div className="card mx-auto my-6 shadow-box w-full max-w-sm">
-            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-                <h2 className="card-title mx-auto">By appointment only</h2>
+            <h2 className="card-title pt-8 mx-auto">By appointment only</h2>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="card-body pt-6"
+            >
                 <p className="form-control">
                     <label htmlFor="name" className="label">
                         <span className="label-text">Full Name</span>
                     </label>
                     <input
-                        {...register("name", { required: true })}
+                        {...register("name")}
                         id="name"
                         type="text"
-                        placeholder="Bike Rider"
+                        inputMode="text"
+                        autoComplete="name"
+                        pattern="^([^0-9]*)$"
+                        // placeholder="Bike Rider"
                         className="input"
                     />
                 </p>
-                {errors.name && <Error err="Name" />}
+                {errors?.name && <Error message={errors?.name?.message} />}
 
                 <p className="form-control">
                     <label htmlFor="email" className="label">
                         <span className="label-text">Email</span>
                     </label>
                     <input
-                        {...register("email", { required: true })}
+                        {...register("email")}
                         id="email"
                         type="email"
-                        placeholder="someone@example.com"
+                        inputMode="email"
+                        autoComplete="email"
+                        // placeholder="someone@example.com"
                         className="input"
                     />
                 </p>
-                {errors.email && <Error err="Email" />}
+                {errors?.email && <Error message={errors?.email?.message} />}
 
                 <p className="form-control">
                     <label htmlFor="phone" className="label">
@@ -79,33 +80,36 @@ const Form = () => {
                     </label>
                     <input
                         {...register("phone", {
-                            required: true,
                             minLength: 10,
                         })}
                         id="phone"
                         type="tel"
-                        placeholder="(555) 123-4567"
+                        inputMode="tel"
+                        autoComplete="tel"
+                        // placeholder="(555) 123-4567"
                         className="input"
                     />
                 </p>
-                {errors.phone && <Error err="Phone" />}
+                {errors?.phone && <Error message={errors?.phone?.message} />}
 
                 <p className="form-control">
                     <label htmlFor="description" className="label">
                         <span className="label-text">
-                            project description - year / make / model
+                            Project Description - Year / Make / Model
                         </span>
                     </label>
                     <textarea
-                        {...register("description", { required: true })}
+                        {...register("description")}
                         id="description"
                         className="textarea textarea-bordered h-24"
-                        placeholder="What can I do for you?"
+                        // placeholder="What can I do for you?"
                     ></textarea>
                 </p>
-                {errors.description && <Error err="Description" />}
+                {errors?.description && (
+                    <Error message={errors?.description?.message} />
+                )}
 
-                <p className="flex gap-6 pt-3">
+                <p className="flex gap-6">
                     <input
                         type="button"
                         value="reset"
@@ -123,22 +127,45 @@ const Form = () => {
     )
 }
 
-// <div className="label flex lg:flex-col gap-6 lg:gap-4 max-lg:mt-6">
-//     <button
-//         type="reset"
-//         className="btn btn-outline btn-primary grow lg:w-full"
-//         onClick={props.handleReset}
-//     >
-//         Reset
-//     </button>
-//     <button
-//         type="submit"
-//         className="btn btn-primary grow lg:w-full"
-//         disabled={props.isSubmitting}
-//         onClick={props.handleSubmit}
-//     >
-//         Submit
-//     </button>
-// </div>
+const Error = ({ message }) => {
+    return (
+        <div className="badge badge-error gap-2 !-mt-8 z-50">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="stroke-current inline-block h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+            </svg>
+            <span className="text-xs">{message}</span>
+        </div>
+    )
+    // return (
+    //     <div className="alert alert-error shadow-box py-2 pl-0 !-my-4">
+    //         <div>
+    //             <svg
+    //                 xmlns="http://www.w3.org/2000/svg"
+    //                 className="stroke-current flex-shrink-0 h-4 w-4"
+    //                 fill="none"
+    //                 viewBox="0 0 24 24"
+    //             >
+    //                 <path
+    //                     strokeLinecap="round"
+    //                     strokeLinejoin="round"
+    //                     strokeWidth="2"
+    //                     d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+    //                 />
+    //             </svg>
+    //             <span className="text-xs">{message}</span>
+    //         </div>
+    //     </div>
+    // )
+}
 
 export default Form
