49f42cb63bfbeabc2fad98d9f9f61e3633c2a63c
cspencer <cspencernd@gmail.com>
2022-12-08 13:50:40 -0500

    fixup all the validations and add cool indicator icons

--- client/src/pages/Contact/Form.jsx
+++ client/src/pages/Contact/Form.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react"
-import { useForm, Controller } from "react-hook-form"
+import { useForm, Controller, useFormState } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import axios from "./axios"
 import "./Form.css"
 import { IMaskInput } from "react-imask"
+import { AlertCircle, CheckCircle } from "react-feather"
 
 const Form = () => {
     const REGISTER_URL = "/send"
 
     const schema = yup.object().shape({
-        name: yup.string().required("Name is a required field"),
+        name: yup
+            .string()
+            .matches(/[A-Za-z]/, "Name must only contain letters")
+            .min(4)
+            .required("Name is a required field"),
         email: yup
             .string()
             .email("Not a valid email")
             .required("Email is a required field"),
-        phone: yup.string().required("Phone is a required field"),
-        description: yup.string().required("Description is a required field"),
+        phone: yup.string().min(14).required("Phone is a required field"),
+        description: yup
+            .string()
+            .min(20)
+            .required("Description is a required field"),
     })
 
     const {
@@ -24,11 +32,24 @@ const Form = () => {
         control,
         handleSubmit,
         reset,
-        formState: { errors, isValid, isSubmitting, isSubmitSuccessful },
+        formState: {
+            errors,
+            isDirty,
+            dirtyFields,
+            isValid,
+            isSubmitting,
+            isSubmitSuccessful,
+        },
     } = useForm({
-        mode: "onChange",
+        mode: "all",
         shouldFocusError: false /** BUG: Needed to prevent crash if phone is empty */,
         resolver: yupResolver(schema),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: "",
+            description: "",
+        },
     })
 
     const [buttonDisabled, setButtonDisabled] = useState("")
@@ -56,13 +77,20 @@ const Form = () => {
         }
     }, [isSubmitting])
 
-    const onSubmit = (data) => {
-        setTimeout(() => console.log(data), 1000)
+    const sleep = (ms) => {
+        return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+
+    const onSubmit = async (data) => {
+        await sleep(2000)
+        console.log(isSubmitting)
+        console.log(data)
+        console.log(isSubmitting)
     }
 
     return (
         <div className="card mx-auto my-6 shadow-box w-full max-w-sm">
-            <h2 className="card-title pt-8 mx-auto">By appointment only</h2>
+            <h2 className="card-title pt-8 mx-auto">Contact</h2>
             <form onSubmit={handleSubmit(onSubmit)} className="card-body pt-6">
                 {/** Name */}
                 <p className="form-control">
@@ -78,8 +106,21 @@ const Form = () => {
                         placeholder=" "
                         className="input"
                     />
+
+                    {dirtyFields.name &&
+                        (errors?.name ? (
+                            <AlertCircle
+                                size={16}
+                                className="text-error absolute right-0 m-4"
+                            />
+                        ) : (
+                            <CheckCircle
+                                size={16}
+                                className="text-success absolute right-0 m-4"
+                            />
+                        ))}
+                    {errors?.name && <Error message={errors?.name?.message} />}
                 </p>
-                {errors?.name && <Error message={errors?.name?.message} />}
 
                 {/** Email */}
                 <p className="form-control">
@@ -95,8 +136,22 @@ const Form = () => {
                         placeholder=" "
                         className="input"
                     />
+                    {dirtyFields.email &&
+                        (errors?.email ? (
+                            <AlertCircle
+                                size={16}
+                                className="text-error absolute right-0 m-4"
+                            />
+                        ) : (
+                            <CheckCircle
+                                size={16}
+                                className="text-success absolute right-0 m-4"
+                            />
+                        ))}
+                    {errors?.email && (
+                        <Error message={errors?.email?.message} />
+                    )}
                 </p>
-                {errors?.email && <Error message={errors?.email?.message} />}
 
                 {/** Phone */}
                 <p className="form-control">
@@ -121,8 +176,22 @@ const Form = () => {
                             />
                         )}
                     />
+                    {dirtyFields.phone &&
+                        (errors?.phone ? (
+                            <AlertCircle
+                                size={16}
+                                className="text-error absolute right-0 m-4"
+                            />
+                        ) : (
+                            <CheckCircle
+                                size={16}
+                                className="text-success absolute right-0 m-4"
+                            />
+                        ))}
+                    {errors?.phone && (
+                        <Error message={errors?.phone?.message} />
+                    )}
                 </p>
-                {errors?.phone && <Error message={errors?.phone?.message} />}
 
                 {/** Description */}
                 <p className="form-control">
@@ -137,10 +206,22 @@ const Form = () => {
                         className="textarea textarea-bordered h-24"
                         placeholder=" "
                     ></textarea>
+                    {dirtyFields.description &&
+                        (errors?.description ? (
+                            <AlertCircle
+                                size={16}
+                                className="text-error absolute right-0 m-4"
+                            />
+                        ) : (
+                            <CheckCircle
+                                size={16}
+                                className="text-success absolute right-0 bottom-0 m-4"
+                            />
+                        ))}
+                    {errors?.description && (
+                        <Error message={errors?.description?.message} />
+                    )}
                 </p>
-                {errors?.description && (
-                    <Error message={errors?.description?.message} />
-                )}
 
                 {/** Buttons */}
                 <p className="flex gap-6">
@@ -169,22 +250,11 @@ const Form = () => {
 
 const Error = ({ message }) => {
     return (
-        <div className="badge badge-error gap-2 !-mt-8 z-10 bg-opacity-75">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="stroke-current inline-block h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-            </svg>
-            <span className="text-xs">{message}</span>
-        </div>
+        <label className="px-1">
+            <span className="label-text-alt text-xs text-error ">
+                {message}
+            </span>
+        </label>
     )
 
     // return (
