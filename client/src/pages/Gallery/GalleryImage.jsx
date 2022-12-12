185c39ee678416733c97e63801e5534abf90c11b
cspencer <cspencernd@gmail.com>
2022-12-12 16:16:03 -0500

    rework the gallery a bit with lazy load image

--- client/src/pages/Gallery/GalleryImage.jsx
+++ client/src/pages/Gallery/GalleryImage.jsx
@@ -2,32 +2,18 @@ import { useState } from "react"
 import Skeleton from "react-loading-skeleton"
 import "react-loading-skeleton/dist/skeleton.css"
 import heroImg from "./motorcycle-work.webp"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const GalleryImage = ({ image, handleClick }) => {
-    const [loaded, setLoaded] = useState(false)
-    const handleLoad = () => setLoaded(true)
-    const isHidden = !loaded ? { display: "none" } : {}
-
     return (
-        <>
-            {!loaded && (
-                <Skeleton
-                    baseColor="hsl(220, 12%, 20%)"
-                    highlightColor="hsl(31, 1%, 30%)"
-                    className="h-56 md:h-64 lg:h-72 xl:h-80"
-                />
-            )}
-            <img
-                src={image[1]}
-                alt={image[0].replaceAll("-", " ")}
-                height="500"
-                width="333"
-                className="rounded-box shadow-box"
-                style={isHidden}
-                onClick={handleClick}
-                onLoad={handleLoad}
-            />
-        </>
+        <LazyLoadImage
+            src={image[1]}
+            alt={image[0].replaceAll("-", " ")}
+            height="500"
+            width="333"
+            className="rounded-box shadow-box"
+            onClick={handleClick}
+        />
     )
 }
 
